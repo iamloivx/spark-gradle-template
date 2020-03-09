d4859edda34782124b7ac5c49ac69056b9a95ba4
hieutmbk <hieutm.bk@gmail.com>
2020-03-09 10:10:37 +0700

    not update

--- src/main/scala/template/spark/CarldataTS.scala
+++ src/main/scala/template/spark/CarldataTS.scala
@@ -3,7 +3,14 @@ import carldata.series.Csv
 
 object Flint extends InitSpark {
   def main(args: Array[String]) = {
-    val file = "file:/D:/spark-gradle-template/dailybalance.csv"
+    val str =
+      "time,value
+        2005-01-01T12:34:15,2
+        2006-01-01T12:34:15,-4
+        2007-01-01T12:34:15,-6
+        2008-01-01T12:34:15,9".stripMargin
+    val series = Csv.fromString(str).head
+    println(series.getClass)
  
     close
   }
